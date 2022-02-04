25f71053845b0d0b8d80500c1e39064cee0a0261
Jay Liu <jminsuliu@gmail.com>
2022-02-04 10:42:43 -0800

    set up __init__.py

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -1,7 +1,14 @@
+"""
+BMI203: Biocomputing algorithms Winter 2022
+Assignment 5: KMeans and Silhouette Scoring
+"""
+
 from .kmeans import KMeans
 from .silhouette import Silhouette
 from .utils import (
         make_clusters, 
         plot_clusters,
         plot_multipanel)
+
+__version__ = '0.1.0'
 
